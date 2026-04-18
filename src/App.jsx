b281c50df64c84fd2a23fa2bcb04774c7ecea2a0
Amira Rabah <amirarabah049@gmail.com>
2026-04-18 13:03:15 +0100

    resolve errors for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import styles from './App.module.css'
 import React, { useState } from 'react'
-import { Navbar } from './components/navbar/Navbar'
+import { Navbar } from './components/Navbar/Navbar'
 import { Hero } from './components/Hero/Hero'
 import { Contact } from './components/Contact/Contact'
 import { Footer } from './components/Footer/Footer'
